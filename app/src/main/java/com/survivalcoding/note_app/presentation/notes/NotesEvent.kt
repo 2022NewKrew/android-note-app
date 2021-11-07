174a2a5c73c15f3e03c00a5318f7d5ca54b6c88e
Junsuk Oh <a811219@gmail.com>
2021-11-07 12:23:24 +0900

    정렬 처리

--- app/src/main/java/com/survivalcoding/note_app/presentation/notes/NotesEvent.kt
+++ app/src/main/java/com/survivalcoding/note_app/presentation/notes/NotesEvent.kt
@@ -4,7 +4,7 @@ import com.survivalcoding.note_app.domain.model.Note
 import com.survivalcoding.note_app.domain.util.NoteOrder
 
 sealed class NotesEvent {
-    //    data class Order(val noteOrder: NoteOrder) : NotesEvent()
+    data class Order(val noteOrder: NoteOrder) : NotesEvent()
     data class DeleteNote(val note: Note) : NotesEvent()
     object RestoreNote : NotesEvent()
 //    object ToggleOrderSection : NotesEvent()
--- app/src/main/java/com/survivalcoding/note_app/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/survivalcoding/note_app/presentation/notes/NotesViewModel.kt
@@ -3,6 +3,8 @@ package com.survivalcoding.note_app.presentation.notes
 import androidx.lifecycle.*
 import com.survivalcoding.note_app.domain.model.Note
 import com.survivalcoding.note_app.domain.repository.NoteRepository
+import com.survivalcoding.note_app.domain.util.NoteOrder
+import com.survivalcoding.note_app.domain.util.OrderType
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
@@ -11,6 +13,7 @@ import kotlinx.coroutines.launch
 class NotesViewModel(
     private val repository: NoteRepository,
 ) : ViewModel(), LifecycleObserver {
+
     private val _state = MutableLiveData(NotesState())
     val state: LiveData<NotesState> = _state
 
@@ -24,13 +27,17 @@ class NotesViewModel(
                     recentlyDeletedNote = event.note
                 }
             }
-//            is NotesEvent.Order -> {
-//                if (state.value.noteOrder::class == event.noteOrder::class &&
-//                        state.value.noteOrder.orderType == event.noteOrder.orderType) {
-//                    return
-//                }
-//                getNotes(event.noteOrder)
-//            }
+            is NotesEvent.Order -> {
+                if (state.value!!.noteOrder::class == event.noteOrder::class &&
+                    state.value!!.noteOrder.orderType == event.noteOrder.orderType
+                ) {
+                    return
+                }
+                _state.value = state.value!!.copy(
+                    noteOrder = event.noteOrder
+                )
+                getNotes()
+            }
             NotesEvent.RestoreNote -> {
                 viewModelScope.launch {
                     repository.insertNote(recentlyDeletedNote ?: return@launch)
@@ -47,9 +54,26 @@ class NotesViewModel(
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     private fun getNotes() {
+        val noteOrder = _state.value!!.noteOrder
+
         repository.getNotes()
             .map { notes ->
-                notes.sortedByDescending { it.timestamp }
+                when (noteOrder.orderType) {
+                    OrderType.Ascending -> {
+                        when (noteOrder) {
+                            is NoteOrder.Color -> notes.sortedBy { it.color }
+                            is NoteOrder.Date -> notes.sortedBy { it.timestamp }
+                            is NoteOrder.Title -> notes.sortedBy { it.title }
+                        }
+                    }
+                    OrderType.Descending -> {
+                        when (noteOrder) {
+                            is NoteOrder.Color -> notes.sortedByDescending { it.color }
+                            is NoteOrder.Date -> notes.sortedByDescending { it.timestamp }
+                            is NoteOrder.Title -> notes.sortedByDescending { it.title }
+                        }
+                    }
+                }
             }
             .onEach { notes ->
                 _state.value = state.value!!.copy(
