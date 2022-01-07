7fa1d70cd083c689c8fecbb55edd15c159211834
Junsuk Oh <a811219@gmail.com>
2022-01-08 00:29:26 +0900

    MutableLiveData -> MutableStateFlow로 변경

--- app/src/main/java/com/survivalcoding/note_app/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/survivalcoding/note_app/presentation/notes/NotesViewModel.kt
@@ -6,6 +6,7 @@ import com.survivalcoding.note_app.domain.repository.NoteRepository
 import com.survivalcoding.note_app.domain.use_case.GetNotesUseCase
 import com.survivalcoding.note_app.domain.util.NoteOrder
 import com.survivalcoding.note_app.domain.util.OrderType
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -15,8 +16,8 @@ class NotesViewModel(
     private val getNotesUseCase: GetNotesUseCase,
 ) : ViewModel(), LifecycleEventObserver {
 
-    private val _state = MutableLiveData(NotesState())
-    val state: LiveData<NotesState> = _state
+    private val _state = MutableStateFlow(NotesState())
+    val state: LiveData<NotesState> = _state.asLiveData()
 
     private var recentlyDeletedNote: Note? = null
 
@@ -41,8 +42,8 @@ class NotesViewModel(
                 }
             }
             NotesEvent.ToggleOrderSection -> {
-                _state.value = state.value!!.copy(
-                    isOrderSectionVisible = !state.value!!.isOrderSectionVisible
+                _state.value = _state.value.copy(
+                    isOrderSectionVisible = !_state.value.isOrderSectionVisible
                 )
             }
         }
@@ -51,7 +52,7 @@ class NotesViewModel(
     private fun getNotes() {
         getNotesUseCase(noteOrder)
             .onEach { notes ->
-                _state.value = state.value!!.copy(
+                _state.value = _state.value.copy(
                     notes = notes,
                 )
             }
