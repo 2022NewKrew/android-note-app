a8417ddcc3748699eee23afaabf98ebccc4eb76a
ian-chu <fn0809@gmail.com>
2022-01-11 21:09:33 +0900

    room datasource 생성

--- app/src/main/java/com/survivalcoding/noteapp/App.kt
+++ app/src/main/java/com/survivalcoding/noteapp/App.kt
@@ -3,16 +3,19 @@ package com.survivalcoding.noteapp
 import android.app.Application
 import androidx.room.Room
 import com.survivalcoding.noteapp.data.datasource.NoteDatabase
+import com.survivalcoding.noteapp.data.datasource.NoteRoomDataSource
 import com.survivalcoding.noteapp.data.repository.NoteRepositoryImpl
 
 class App : Application() {
     val repository by lazy {
         NoteRepositoryImpl(
-            Room.databaseBuilder(
-                applicationContext,
-                NoteDatabase::class.java,
-                "TodoDatabase"
-            ).build().noteDao()
+            NoteRoomDataSource(
+                Room.databaseBuilder(
+                    applicationContext,
+                    NoteDatabase::class.java,
+                    "NoteDatabase"
+                ).build().noteDao()
+            )
         )
     }
 }
--- app/src/main/java/com/survivalcoding/noteapp/data/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/survivalcoding/noteapp/data/repository/NoteRepositoryImpl.kt
@@ -1,23 +1,23 @@
 package com.survivalcoding.noteapp.data.repository
 
-import com.survivalcoding.noteapp.data.datasource.NoteDao
+import com.survivalcoding.noteapp.data.datasource.NoteDataSource
 import com.survivalcoding.noteapp.domain.model.Note
 import com.survivalcoding.noteapp.domain.repository.NoteRepository
 
-class NoteRepositoryImpl(private val dao: NoteDao) : NoteRepository {
-    override suspend fun insertNote(note: Note) = dao.insertNote(note)
+class NoteRepositoryImpl(private val dataSource: NoteDataSource) : NoteRepository {
+    override suspend fun insertNote(note: Note) = dataSource.insertNote(note)
 
-    override suspend fun deleteNote(note: Note) = dao.deleteNote(note)
+    override suspend fun deleteNote(note: Note) = dataSource.deleteNote(note)
 
-    override suspend fun sortByColorAsc(): List<Note> = dao.sortByColorAsc()
+    override suspend fun sortByColorAsc(): List<Note> = dataSource.sortByColorAsc()
 
-    override suspend fun sortByColorDesc(): List<Note> = dao.sortByColorDesc()
+    override suspend fun sortByColorDesc(): List<Note> = dataSource.sortByColorDesc()
 
-    override suspend fun sortByTitleAsc(): List<Note> = dao.sortByTitleAsc()
+    override suspend fun sortByTitleAsc(): List<Note> = dataSource.sortByTitleAsc()
 
-    override suspend fun sortByTitleDesc(): List<Note> = dao.sortByTitleDesc()
+    override suspend fun sortByTitleDesc(): List<Note> = dataSource.sortByTitleDesc()
 
-    override suspend fun sortByTimestampAsc(): List<Note> = dao.sortByTimestampAsc()
+    override suspend fun sortByTimestampAsc(): List<Note> = dataSource.sortByTimestampAsc()
 
-    override suspend fun sortByTimestampDesc(): List<Note> = dao.sortByTimestampDesc()
+    override suspend fun sortByTimestampDesc(): List<Note> = dataSource.sortByTimestampDesc()
 }
