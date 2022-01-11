abd972fdadeeebd6dcddb3b7e4349d878e470d0d
peter.shlee <peter.shlee@kakaocorp.com>
2022-01-11 19:10:45 +0900

    Application class에서 repository, data source, dao 객체 생성

--- app/src/main/java/com/survivalcoding/noteapp/App.kt
+++ app/src/main/java/com/survivalcoding/noteapp/App.kt
@@ -1,7 +1,25 @@
 package com.survivalcoding.noteapp
 
 import android.app.Application
+import androidx.room.Room
+import com.survivalcoding.noteapp.data.data_source.NoteDao
+import com.survivalcoding.noteapp.data.data_source.NoteDatabase
+import com.survivalcoding.noteapp.data.data_source.NoteLocalDataSource
+import com.survivalcoding.noteapp.data.data_source.NoteRoomDataSource
+import com.survivalcoding.noteapp.data.repository.NoteRepositoryImpl
+import com.survivalcoding.noteapp.domain.repository.NoteRepository
 
 class App : Application() {
+    val noteRepository: NoteRepository by lazy {
+        NoteRepositoryImpl(noteLocalDataSource)
+    }
 
+    private val noteLocalDataSource: NoteLocalDataSource by lazy {
+        NoteRoomDataSource(noteDao)
+    }
+
+    private val noteDao: NoteDao by lazy {
+        Room.databaseBuilder(this, NoteDatabase::class.java, NoteDatabase.NOTE_DATABASE_NAME)
+            .build().noteDao()
+    }
 }
--- app/src/main/java/com/survivalcoding/noteapp/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/survivalcoding/noteapp/data/data_source/NoteDatabase.kt
@@ -1,4 +1,13 @@
 package com.survivalcoding.noteapp.data.data_source
 
-abstract class NoteDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [NoteDto::class], version = 1)
+abstract class NoteDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        const val NOTE_DATABASE_NAME = "note-database"
+    }
 }
