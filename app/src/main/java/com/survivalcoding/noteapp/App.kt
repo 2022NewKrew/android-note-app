a41a1dbb69c92964e9e6d07944de354f549b9eea
ian-chu <fn0809@gmail.com>
2022-01-11 20:49:09 +0900

    repository instance 생성

--- app/src/main/java/com/survivalcoding/noteapp/App.kt
+++ app/src/main/java/com/survivalcoding/noteapp/App.kt
@@ -1,7 +1,18 @@
 package com.survivalcoding.noteapp
 
 import android.app.Application
+import androidx.room.Room
+import com.survivalcoding.noteapp.data.datasource.NoteDatabase
+import com.survivalcoding.noteapp.data.repository.NoteRepositoryImpl
 
 class App : Application() {
-
+    val repository by lazy {
+        NoteRepositoryImpl(
+            Room.databaseBuilder(
+                applicationContext,
+                NoteDatabase::class.java,
+                "TodoDatabase"
+            ).build().noteDao()
+        )
+    }
 }
