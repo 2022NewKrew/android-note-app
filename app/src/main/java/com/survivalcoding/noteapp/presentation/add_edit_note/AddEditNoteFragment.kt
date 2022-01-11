528ef0d0a428683855bb609fb6af9c2f4e7279d1
fac.toriall <fac.toriall@kakaocorp.com>
2022-01-11 19:18:42 +0900

    간단한 화면 이동 구현

--- app/src/main/java/com/survivalcoding/noteapp/presentation/add_edit_note/AddEditNoteFragment.kt
+++ app/src/main/java/com/survivalcoding/noteapp/presentation/add_edit_note/AddEditNoteFragment.kt
@@ -1,19 +1,39 @@
 package com.survivalcoding.noteapp.presentation.add_edit_note
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.survivalcoding.noteapp.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.survivalcoding.noteapp.App
+import com.survivalcoding.noteapp.databinding.FragmentAddEditNoteBinding
 
 class AddEditNoteFragment : Fragment() {
+    private var _binding: FragmentAddEditNoteBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by viewModels<AddEditNoteViewModel> {
+        AddEditNoteViewModelFactory(
+            notesRepository = (requireActivity().application as App).notesRepository,
+            owner = this,
+            defaultArgs = arguments
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit_note, container, false)
+    ): View {
+        _binding = FragmentAddEditNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //ToDo: 수정 및 추가 기능 구현
+        val saveButton = binding.saveButton
+        saveButton.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
     }
 }
--- app/src/main/java/com/survivalcoding/noteapp/presentation/notes/NotesFragment.kt
+++ app/src/main/java/com/survivalcoding/noteapp/presentation/notes/NotesFragment.kt
@@ -1,19 +1,62 @@
 package com.survivalcoding.noteapp.presentation.notes
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.survivalcoding.noteapp.App
 import com.survivalcoding.noteapp.R
+import com.survivalcoding.noteapp.databinding.FragmentNotesBinding
+import com.survivalcoding.noteapp.presentation.add_edit_note.AddEditNoteFragment
 
 class NotesFragment : Fragment() {
+    private var _binding: FragmentNotesBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by activityViewModels<NotesViewModel> {
+        NotesViewModelFactory(
+            application = requireActivity().application,
+            notesRepository = (requireActivity().application as App).notesRepository
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes, container, false)
+        _binding = FragmentNotesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //ToDo: RecyclerView 구현 및 관련 클래스 규정
+
+        val alignButton = binding.alignButton
+        val filterLayout = binding.filterLayout
+        alignButton.setOnClickListener {
+            if (filterLayout.visibility == VISIBLE) filterLayout.visibility = GONE
+            else filterLayout.visibility = VISIBLE
+        }
+        val addButton = binding.addButton
+        addButton.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(
+                    R.id.fragment_container_view,
+                    AddEditNoteFragment().apply {
+                        this.arguments = bundleOf(MODIFY to -1)
+                    })
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
+    companion object {
+        const val MODIFY = "modify"
     }
 }
