cb1b484d80aeb9e06595eb8cdd077e2472e3a2d5
fac.toriall <fac.toriall@kakaocorp.com>
2022-01-11 18:40:58 +0900

    UI 관련 xml 추가

--- app/src/main/java/com/survivalcoding/noteapp/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/survivalcoding/noteapp/presentation/notes/NotesViewModel.kt
@@ -4,15 +4,15 @@ import android.app.Application
 import androidx.lifecycle.*
 import com.survivalcoding.noteapp.domain.model.Note
 import com.survivalcoding.noteapp.domain.repository.NotesRepository
+import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.launch
 
 class NotesViewModel(
     application: Application,
     private val notesRepository: NotesRepository
 ) : AndroidViewModel(application) {
-    val notes: LiveData<List<Note>> = notesRepository.getNotes().asLiveData()
-    private var _deletedNote = MutableLiveData<Note?>()
-    val deletedNote: LiveData<Note?> = _deletedNote
+    private var notes: LiveData<List<Note>> = notesRepository.getNotes().asLiveData()
+    private var deletedNote : Note? = null
 
     suspend fun getNoteById(id: Int): Note? = notesRepository.getNoteById(id)
 
@@ -25,16 +25,25 @@ class NotesViewModel(
     fun deleteNote(note: Note) {
         viewModelScope.launch {
             notesRepository.deleteNote(note)
-            _deletedNote.value = note
+            deletedNote = note
         }
     }
 
     fun restoreNote() {
         viewModelScope.launch {
-            _deletedNote.value?.let {
+            deletedNote?.let {
                 notesRepository.insertNote(it)
             }
-            _deletedNote.value = null
+            deletedNote = null
+        }
+    }
+
+    fun filterNotes(filter: Int, sort: Int){
+        viewModelScope.launch{
+            //LiveData는 _notes.value = filtering() 이런 식으로 알려줄 수 있는데
+            //Flow는 어떻게??
+            //생각나는건 NoteDao에 filtering 관련으로 정렬된 채 오는 쿼리문을 작성해서 이를 Flow로 받게 하는 것
+            //근데 이게 과연 효율적인가??
         }
     }
 }
