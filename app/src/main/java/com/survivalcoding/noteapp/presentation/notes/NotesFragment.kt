539d898d714fd87d4aacc7225ee7c03ee13e0ace
ian-chu <fn0809@gmail.com>
2022-01-13 15:19:06 +0900

    note list 화면 구현

--- app/src/main/java/com/survivalcoding/noteapp/presentation/notes/NotesFragment.kt
+++ app/src/main/java/com/survivalcoding/noteapp/presentation/notes/NotesFragment.kt
@@ -1,13 +1,18 @@
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import androidx.fragment.app.viewModels
 import com.survivalcoding.noteapp.App
+import com.survivalcoding.noteapp.R
 import com.survivalcoding.noteapp.databinding.FragmentNotesBinding
 import com.survivalcoding.noteapp.presentation.notes.NotesViewModel
 import com.survivalcoding.noteapp.presentation.notes.NotesViewModelFactory
+import com.survivalcoding.noteapp.presentation.notes.adapter.NoteListAdapter
 
 class NotesFragment : Fragment() {
 
@@ -15,6 +20,11 @@ class NotesFragment : Fragment() {
     private val binding get() = _binding!!
     private val viewModel by viewModels<NotesViewModel> {
         NotesViewModelFactory((requireActivity().application as App).repository)
+    }
+    private val adapter by lazy {
+        NoteListAdapter({ todo -> viewModel.deleteNote(todo) }, { todo ->
+            moveToAdd()
+        })
     }
 
     override fun onCreateView(
@@ -26,8 +36,34 @@ class NotesFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // recyclerView 설정
+        binding.notesRecyclerView.adapter = adapter
+
+        // 작성하기 화면으로 이동
+        binding.notesFabAdd.setOnClickListener { moveToAdd() }
+
+        // note list 업데이트 관찰
+        viewModel.notes.observe(this) { list ->
+            adapter.submitList(list)
+        }
+
+        // 할일 목록 조회
+        viewModel.getNotes()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun moveToAdd() {
+        parentFragmentManager.commit {
+            replace<AddEditFragment>(R.id.main_fragment_container_view)
+            setReorderingAllowed(true)
+            addToBackStack(null) // name can be null
+        }
     }
 }
---
+++ app/src/main/java/com/survivalcoding/noteapp/presentation/notes/adapter/NoteViewHolder.kt
@@ -0,0 +1,21 @@
+package com.survivalcoding.noteapp.presentation.notes.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.survivalcoding.noteapp.databinding.ItemNoteBinding
+import com.survivalcoding.noteapp.domain.model.Note
+
+class NoteViewHolder(
+    private val binding: ItemNoteBinding,
+    private val deleteClickListener: (Note) -> Unit,
+    private val itemClickListener: (Note) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(note: Note) {
+        binding.noteTvTitle.text = note.title
+        binding.noteTvContent.text = note.content
+        binding.root.setBackgroundColor(note.color)
+
+        binding.noteIvDelete.setOnClickListener { deleteClickListener(note) }
+        binding.root.setOnClickListener { itemClickListener(note) }
+    }
+}
