90e7ee109b18522430c184f67bf5595233efc62f
ian-chu <fn0809@gmail.com>
2022-01-13 23:11:08 +0900

    노트 색상 변경 기능 구현

--- app/src/main/java/com/survivalcoding/noteapp/presentation/addedit/AddEditViewModel.kt
+++ app/src/main/java/com/survivalcoding/noteapp/presentation/addedit/AddEditViewModel.kt
@@ -1,6 +1,7 @@
 package com.survivalcoding.noteapp.presentation.addedit
 
 import androidx.lifecycle.*
+import com.survivalcoding.noteapp.domain.model.Color
 import com.survivalcoding.noteapp.domain.model.Note
 import com.survivalcoding.noteapp.domain.usecase.InsertNoteUseCase
 import kotlinx.coroutines.launch
@@ -8,8 +9,14 @@ import kotlinx.coroutines.launch
 class AddEditViewModel(private val insertNoteUseCase: InsertNoteUseCase) : ViewModel() {
     private val _note = MutableLiveData<Note>()
     val note: LiveData<Note> get() = _note
+    private val _color = MutableLiveData(Color.ORANGE)
+    val color: LiveData<Color> get() = _color
 
     fun insertNote(note: Note) = viewModelScope.launch {
         insertNoteUseCase(note)
+    }
+
+    fun changeColor(color: Color) {
+        _color.value = color
     }
 }
